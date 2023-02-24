f11a14d716d03115d310c9c82c620e7ac5783e45
maDwaZz <madwazz1@gmail.com>
2023-02-24 23:43:45 +0300

    SkillFactory D8.6

---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import NewsList, NewsDetail, NewsSearch, NewsCreate, NewsUpdate, NewsDelete, upgrade_me
+
+urlpatterns = [
+    path('', NewsList.as_view(), name='news_list'),
+    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
+    path('search/', NewsSearch.as_view(), name='news_search'),
+    path('create/', NewsCreate.as_view(), name='news_create'),
+    path('<int:pk>/update/', NewsUpdate.as_view(), name='news_update'),
+    path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+]
